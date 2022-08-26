5fc380306d13271a7d27e3d52290986bfc592023
Muhammad Dinislam <ariyaan5492@gmail.com>
2022-08-26 13:22:49 +0600

    Practice

--- Practice.c
+++ Practice.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
+#include <windows.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 int main ()
 {
-  int a,b;
-  scanf("%d %d",&a,&b);
-  char num[100][100]={"one","two","three","four","five","six","seven","eight","nine","even","odd"};
-  int store,i;
-  for(i=a; i<=b; i++)
-  {
-    if(i<10)
+    int Hour,Minute,Second;
+    printf("Set Your Time:\n");
+    scanf("%d%d%d",&Hour,&Minute,&Second);
+    if(Hour > 12 || Minute > 60 || Second > 60)
     {
-      store=i-1;
+        printf(" Error! ");
+        exit(0);
     }
-    else if(i>9 && i%2==0)
+    while(1)
     {
-      store=9+i%2;
+        system("cls");
+        fflush(stdout);
+        Second++;
+        if(Second > 59)
+        {
+            Minute++;
+            Second = 0;
+        }
+        if(Minute > 59)
+        {
+            Hour++;
+            Minute = 0;
+        }
+        if(Hour > 12)
+        {
+            Hour = 1;
+        }
+        printf("*** Digital Clock ***\n");
+        printf("\n   %.2d : %.2d : %.2d",Hour,Minute,Second);
+        if(Hour < 12 && Minute <= 59)
+        {
+            printf("  PM");
+        }
+        else
+        {
+            printf("  AM");
+        }
+        sleep(1);
     }
-    else if(i>9 && i%2!=0)
-    {
-      store=9+i%2;
-    }
-    printf("%s\n",num[store]);
-  }
-  return 0;
+    return 0;
 }
